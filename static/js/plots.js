fcea2204ab7b2889d1612a64ed40c260090cfca9
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-07-28 19:33:31 -0400

    Changed size of plots

--- static/js/plots.js
+++ static/js/plots.js
@@ -226,8 +226,8 @@ var data = [trace1 = {x:xArray, y:yArray, xaxis: 'x', yaxis: 'y', mode:"markers"
   var layout = {
     grid: {rows: 3, columns: 3, pattern: 'independent'},
     autosize: false,
-    width: 1500,
-    height: 1500,
+    width: 1000,
+    height: 1000,
     annotations: [{
       text: "Women: Age Vs. Salary",
         font: {
